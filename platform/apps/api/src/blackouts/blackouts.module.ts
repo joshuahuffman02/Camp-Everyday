ea01d781a369d7fc5da1b427e28fd317bad5dbaa
joshuahuffman02 <129413071+joshuahuffman02@users.noreply.github.com>
2026-01-02 13:52:34 -0600

    Fix PermissionsModule import for blackouts

--- platform/apps/api/src/blackouts/blackouts.module.ts
+++ platform/apps/api/src/blackouts/blackouts.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { BlackoutsService } from "./blackouts.service";
 import { BlackoutsController } from "./blackouts.controller";
 import { PrismaModule } from "../prisma/prisma.module";
+import { PermissionsModule } from "../permissions/permissions.module";
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, PermissionsModule],
     controllers: [BlackoutsController],
     providers: [BlackoutsService],
     exports: [BlackoutsService]
